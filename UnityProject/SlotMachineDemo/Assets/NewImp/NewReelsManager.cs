d5eb15acb866ef8acb76e058a999763df4bcfd1a
Leonardo Martin Albalah <leoalbalah@gmail.com>
2022-05-24 14:33:38 +0100

    Reels  Stopping Gradually

--- UnityProject/SlotMachineDemo/Assets/NewImp/NewReelsManager.cs
+++ UnityProject/SlotMachineDemo/Assets/NewImp/NewReelsManager.cs
@@ -7,6 +7,37 @@ public class NewReelsManager : MonoBehaviour
 
     public NewReel[] reels;
 
+    public bool mustStop;
+    private ArrayList _results = new ArrayList();
+
+    private void Update()
+    {
+        if (mustStop)
+        {
+            if (!reels[reels.Length - 1].isSpinning)
+            {
+                mustStop = false;
+                gameManager.ShowResults();
+                _results = new ArrayList();
+            }
+            else
+            {
+                for (int i = 0; i < _results.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        reels[i].stopOn = (int)_results[i];
+                    }
+                    else if (!reels[i - 1].isSpinning)
+                    {
+                        reels[i].stopOn = (int)_results[i];
+                        _results[i - 1] = -1;
+                    }
+                }
+            }
+        }
+    }
+
     public void Spin()
     {
         for (var i = 0; i < reels.Length; i++)
@@ -17,11 +48,7 @@ public class NewReelsManager : MonoBehaviour
 
     public void Stop(ArrayList results)
     {
-        for (int i = 0; i < results.Count; i++)
-        {
-            reels[i].stopOn = (int)results[i];
-        }
-
-        gameManager.ShowResults();
+        _results = results;
+        mustStop = true;
     }
 }
